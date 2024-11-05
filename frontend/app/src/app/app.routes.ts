cd5fbedbeb0c7342a4894f611934d4cc370e1934
sabbagh <mohammad-wael.sabbagh6@etu.univ-lorraine.fr>
2024-11-05 15:45:15 +0100

    create timeTable

--- frontend/app/src/app/app.routes.ts
+++ frontend/app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TimetableComponent } from './timetable/timetable.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //{path: '', redirectTo: 'timetable', pathMatch: 'full'},
+    {path: 'timetable', component: TimetableComponent},
+];
--- frontend/app/src/app/scripts/API/APITypes/Events.ts
+++ frontend/app/src/app/scripts/API/APITypes/Events.ts
@@ -1,11 +1,32 @@
 export type EventType = {
-
+    id: string;
+    title: string;
+    description?: string;
+    startDate: Date;
+    endDate?: Date;
+    dayLong?: boolean;
+    recurrence?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    user?: string;
 }
 
 export type EventInPatchType = {
-
+    title?: string;
+    description?: string;
+    startDate?: Date;
+    endDate?: Date;
+    dayLong?: boolean;
+    recurrence?: string;
+    user?: string;
 }
 
 export type EventInCreateType = {
-
+    title: string;
+    description?: string;
+    startDate: Date;
+    endDate: Date;
+    dayLong: boolean;
+    recurrence?: string;
+    user?: string;
 }
