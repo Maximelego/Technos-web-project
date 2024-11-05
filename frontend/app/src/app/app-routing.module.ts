f01ae116af7ed12e20f148f6af05e36bbf8f7313
sabbagh <mohammad-wael.sabbagh6@etu.univ-lorraine.fr>
2024-11-05 23:00:05 +0100

    add timetable route

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { authGuard } from './auth.guard';
+import { TimetableComponent } from './timetable/timetable.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] }
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  {path: 'timetable', component: TimetableComponent},
 ];
 
 @NgModule({
